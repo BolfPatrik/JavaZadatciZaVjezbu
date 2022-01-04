e425be01ac87812ef9451bce8c9a374aea83331d
BolfPatrik <bolfpatrik0@gmail.com>
2022-01-04 23:42:46 +0100

    Create Zadatak7.java

---
+++ Zadatak7.java
@@ -0,0 +1,20 @@
+package edunova;
+public class Zadatak8 {
+public static void main(String[] args) {
+	boolean prim=true;
+	int suma=0;
+	for(int t=2;t<=100;t++) {
+		prim=true;
+		for(int i=2;i<t;i++) {
+		if(t%i==0) {
+			prim=false;
+			break;
+		}
+		}
+		if(prim) {
+			suma+=t;
+		}
+	}
+	System.out.println(suma);
+}
+}
